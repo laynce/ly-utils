9e513420d9f99f6efe72c82a874fcb2b0379f487
laynce <673413129@qq.com>
2023-06-26 15:41:15 +0800

    Update format.js

--- src/tool/format.js
+++ src/tool/format.js
@@ -13,7 +13,42 @@ const formatFileSize = (fileSize, unit) => {
   }
   return `${(size / (1024 * 1024 * 1024)).toFixed(2)} TB`
 }
+// 获取 壹到壹佰
+const getCnLabels = ()=> {
+    const labels = {
+        1: '壹', 
+        2: '贰', 
+        3: '叁', 
+        4: '肆', 
+        5: '伍', 
+        6: '陆', 
+        7: '柒', 
+        8: '捌', 
+        9: '玖', 
+        10: '拾', 
+        100: '壹佰'
+    }
+
+    const arr = []
+
+    for(let i = 0; i< 100; i++) {
+        let val = String(i + 1)
+
+        if (!labels[i + 1] && /^(\d)(\d)$/.test(val)) {
+            val = val.replace(/^(\d)(\d)$/, (...r)=> {
+                return r[1] > 1 ? `${labels[r[1]]}拾${labels[r[2]] || ''}` : `拾${labels[r[2]] || ''}`
+            })
+  
+            arr.push(val)
+        }else {
+            arr.push(labels[i + 1])
+        }
+    }
+    
+    return arr
+  }
 
 export {
-  formatFileSize
+  formatFileSize,
+  getCnLabels
 }
